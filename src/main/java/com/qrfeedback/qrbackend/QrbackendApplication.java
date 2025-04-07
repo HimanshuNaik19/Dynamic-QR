7372edab0e9fe86300c7e63e1df8c735621ee484
HimanshuNaik19 <naikhimanshu235@gmail.com>
2025-04-07 16:57:56 +0530

    first commit

---
+++ src/main/java/com/qrfeedback/qrbackend/QrbackendApplication.java
@@ -0,0 +1,13 @@
+package com.qrfeedback.qrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qrfeedback/qrbackend/QrbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrfeedback.qrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
